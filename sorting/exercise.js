6e618383bb21afdc2f829dd79dc0e381a21e7bc8
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-09-04 18:36:57 -0400

    added sorting

---
+++ sorting/exercise.js
@@ -0,0 +1,34 @@
+/**
+ * bubble sort: x
+ * selection sort: x
+ * insertion sort: nearly sorted or small list
+ * merge sort: t>s
+ * quick sort: s>t
+ *
+ * radix / counting sort: must be numbers and in range
+ */
+
+//#1 - Sort 10 schools around your house by distance:
+// ans: any
+
+//#2 - eBay sorts listings by the current Bid amount:
+// old ans: merge or insertion sort
+// ans: radix or counting sort
+
+//#3 - Sport scores on ESPN
+// ans: quick sort
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+// ans: merge sort (probably not going to sort in memory and more concern about worst case in quick sort)
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+// ans: insertion sort
+
+//#6 - Temperature Records for the past 50 years in Canada
+// ans: quick sort
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+// ans: quick sort
+
+//#8 - You want to teach sorting for the first time
+// ans: bubble or selection sort
